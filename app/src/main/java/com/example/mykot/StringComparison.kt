500f8f0896654aca1ac70d3d095f21dea77d021d
vtscpdo <vtscpdo@visteon.com>
2020-02-24 16:40:31 +0530

    String comparison in Kotlin

---
+++ app/src/main/java/com/example/mykot/StringComparison.kt
@@ -0,0 +1,12 @@
+package com.example.mykot
+
+fun main() {
+
+    var name1 = "Vikash"
+    var name2 = "Vikash"
+
+    // println(name1.equals(name2))  // Java
+    //  println(name1 == name2)
+
+    if (name1 == name2) println("It's Same") else println("It's not Same")
+}
