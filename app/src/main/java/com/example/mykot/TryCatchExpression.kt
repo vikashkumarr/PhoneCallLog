b0bf21fae0313368ddbb0b1ae0ba54a3f1af5589
vtscpdo <vtscpdo@visteon.com>
2020-02-25 19:25:48 +0530

    try catch expression in Kotlin

---
+++ app/src/main/java/com/example/mykot/TryCatchExpression.kt
@@ -0,0 +1,23 @@
+package com.example.mykot
+
+import java.lang.Exception
+
+fun main() {
+
+    var str = "4a" // 44
+    //  var num: Int = 0
+
+    try {
+        println(str.toInt() + 10)
+    } catch (e: Exception) {
+        println("Exception Occurred")
+    }
+
+// try with single expression
+    var num = try {
+        str.toInt()
+    } catch (e: NumberFormatException) {
+        println("Exception NumberFormatException")
+    }
+    println(num)
+}
